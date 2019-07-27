8179ba9d3b5a694d9b24561932e84d2a681049cf
AlexHotBogomol <zakablukov777@gmail.com>
2019-07-27 13:34:47 +0300

    fix anotherProgrammer.php

--- solid/AnotherProgrammer.php
+++ solid/AnotherProgrammer.php
@@ -1,30 +1,54 @@
 <?php
-
 //Hint - Open Closed Principle
-class AnotherProgrammer
+abstract class Worker{
+    abstract protected function doWork();
+}
+class AnotherProgrammer extends Worker
 {
     public function code()
     {
         return 'coding';
     }
+
+    public function doWork(){
+        return $this->code();
+    }
 }
-class Tester
+class Tester extends Worker
 {
     public function test()
     {
         return 'testing';
     }
+
+    public function doWork(){
+        return $this->test();
+    }
 }
+class Designer extends Worker
+{
+    public function draw()
+    {
+        return 'drawing';
+    }
+
+    public function doWork(){
+        return $this->draw();
+    }
+}
+/** Что если добавить еще класс Designer с методом draw() **/
 class ProjectManagement
 {
-    public function process($member)
+    public function process(Worker $member)
     {
-        if ($member instanceof AnotherProgrammer) {
-            $member->code();
-        } elseif ($member instanceof Tester) {
-            $member->test();
-        };
-        throw new Exception('Invalid input member');
+        return $member->doWork();
     }
 }
 
+$programmer = new AnotherProgrammer();
+$tester = new Tester();
+$designer = new Designer();
+$project_management = new ProjectManagement();
+echo $project_management -> process($programmer);
+echo $project_management -> process($tester);
+echo $project_management -> process($designer);
